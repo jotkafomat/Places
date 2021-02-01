1f9ff754708490af83df56e3e064cb8be9a4a71f
jotkafomat <k-j@xl.wp.pl>
2021-02-01 17:18:52 +0000

    add cluster name and color

--- InterestingPlaces/Views/MapViewUI.swift
+++ InterestingPlaces/Views/MapViewUI.swift
@@ -55,17 +55,33 @@ struct MapViewUI: UIViewRepresentable {
     final class MapCoordinator: NSObject, MKMapViewDelegate {
         
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            guard let placeAnnotation = annotation as? Place else {
-                return nil
+            
+            switch annotation {
+            case let cluster as MKClusterAnnotation:
+                let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "cluster") as? MKMarkerAnnotationView ?? MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "cluster")
+                annotationView.markerTintColor = .brown
+                for clusterAnnotation in cluster.memberAnnotations {
+                    if let place = clusterAnnotation as? Place {
+                        if place.sponsored  {
+                            cluster.title = place.name
+                            break
+                        }
+                    }
+                }
+                annotationView.titleVisibility = .visible
+                return annotationView
+            case let placeAnnotation as Place:
+                let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "Interesting Place") as? MKMarkerAnnotationView ?? MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "Interesting Place")
+                annotationView.canShowCallout = true
+                annotationView.glyphText = "🔍"
+                annotationView.clusteringIdentifier = "cluster"
+                annotationView.glyphTintColor = UIColor(displayP3Red: 0.082, green: 0.518, blue: 0.263, alpha: 1.0)
+                annotationView.titleVisibility = .visible
+                annotationView.detailCalloutAccessoryView = UIImage(named: placeAnnotation.image).map(UIImageView.init)
+                return annotationView
+            default: return nil
+                
             }
-            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "Interesting Place") as? MKMarkerAnnotationView ?? MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "Interesting Place")
-            annotationView.canShowCallout = true
-            annotationView.glyphText = "🔍"
-            annotationView.glyphTintColor = UIColor(displayP3Red: 0.082, green: 0.518, blue: 0.263, alpha: 1.0)
-            annotationView.titleVisibility = .visible
-            annotationView.detailCalloutAccessoryView = UIImage(named: placeAnnotation.image).map(UIImageView.init)
-            return annotationView
         }
-        
     }
 }
